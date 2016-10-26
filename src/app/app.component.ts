46aae32a9e97e803e3787441b606fccedb5b1d60
Harold Rodriguez <Harold.rodriguez@aztec.com.au>
2016-10-26 14:55:24 +1100

    Added static list of heroes to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,30 @@ import '../../public/css/styles.css';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent { 
-  hero: Hero  = {
-    id: 1,
-    name: 'Windstorm'
-  };
+export class AppComponent {
+  title: 'Tour of heroes';
+  heroes = heroes; 
+  selectedHero: Hero;
+
+  onSelect(hero: Hero){
+    this.selectedHero = hero;
+  }
 }
 
 export class Hero {
   id: number;
   name: string;
 }
+
+const heroes: Array<Hero> = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+]; 
